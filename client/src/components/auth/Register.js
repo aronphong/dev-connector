b1007b600636b87c87db9d73a5f393f9ea37f521
Aron Phong <aronphong@gmail.com>
2020-08-27 06:35:03 -0700

    Fix login link

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+// import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +15,28 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // };
+      // try {
+      //   const config = {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post("/api/users", body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
     }
   };
 
@@ -28,14 +46,14 @@ const Register = () => {
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' onSubmit={(e) => onSubmit(e)}>
+      <form className='form' onSubmit={onSubmit}>
         <div className='form-group'>
           <input
             type='text'
             placeholder='Name'
             name='name'
             value={name}
-            onChange={(e) => onChange(e)}
+            onChange={onChange}
             required
           />
         </div>
@@ -44,7 +62,7 @@ const Register = () => {
             type='email'
             placeholder='Email Address'
             value={email}
-            onChange={(e) => onChange(e)}
+            onChange={onChange}
             name='email'
             required
           />
@@ -60,7 +78,7 @@ const Register = () => {
             name='password'
             minLength='6'
             value={password}
-            onChange={(e) => onChange(e)}
+            onChange={onChange}
           />
         </div>
         <div className='form-group'>
@@ -70,13 +88,13 @@ const Register = () => {
             name='password2'
             minLength='6'
             value={password2}
-            onChange={(e) => onChange(e)}
+            onChange={onChange}
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/register'>Sign In</Link>
       </p>
     </Fragment>
   );
