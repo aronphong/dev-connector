a9dd5bfba99602e1f424ec4ae6fc6d8162c2d089
Aron Phong <aronphong@gmail.com>
2020-09-10 06:25:26 -0700

    Create Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
@@ -94,6 +95,23 @@ export const removeLike = (id) => async (dispatch) => {
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
     });
   } catch (err) {
     dispatch({
